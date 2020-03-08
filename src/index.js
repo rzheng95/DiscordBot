3fda045033e8e3108b32e72215e243abcd9bd8a3
rzheng95 <ruixiangzheng95@gmail.com>
2020-03-08 01:15:15 -0500

    Initialized Discord client.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ const Discord = require('discord.js');
 
 const { prefix, token } = require('./config.json');
 
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!')
+});
+
+client.login(token);
